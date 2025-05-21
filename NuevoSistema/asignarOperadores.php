8cdbb1f2ba9a01e9ac9c674d8e2ac18672174250
jarmandoR <jose523a@gmail.com>
2025-05-21 14:41:11 -0500

    wip:Optimizacion de asignacion

--- NuevoSistema/asignarOperadores.php
+++ NuevoSistema/asignarOperadores.php
@@ -42,41 +42,33 @@ for ($b = 1; $b <= $registros; $b++) {
         $idsProcesados[] = $idser;
 
         // 🔍 UPDATE cuentaspromotor
-        // $t2 = microtime(true);
-        // $sql1 = "UPDATE cuentaspromotor SET cue_idoperentrega = ?, cue_fecha = ?, cue_estado = '9' WHERE cue_idservicio = ?";
-        // $db->query($sql1, [$param31, $fechatiempo, $idser]);
-        // $t3 = microtime(true);
-		echo$sql1="UPDATE `cuentaspromotor` SET `cue_idoperentrega`='$param31', `cue_fecha`='$fechatiempo', cue_estado='9'  where cue_idservicio=$idser";
-
-        // echo "⏱️ UPDATE cuentaspromotor: " . round($t3 - $t2, 4) . " segundos<br>";
+        $t2 = microtime(true);
+        $sql1 = "UPDATE cuentaspromotor SET cue_idoperentrega = ?, cue_fecha = ?, cue_estado = '9' WHERE cue_idservicio = ?";
+        $db->query($sql1, [$param31, $fechatiempo, $idser]);
+        $t3 = microtime(true);
+        echo "⏱️ UPDATE cuentaspromotor: " . round($t3 - $t2, 4) . " segundos<br>";
 
         // 🔍 UPDATE servicios
-        // $t4 = microtime(true);
-        // $sql2 = "UPDATE servicios SET ser_idusuarioguia = ?, ser_idusuarioregistro = ?, ser_fechaguia = ?, ser_estado = '9', ser_visto = 0 WHERE idservicios = ?";
-        // $db->query($sql2, [$param31, $id_usuario, $fechatiempo, $idser]);
-        // $t5 = microtime(true);
-        echo$sql2="UPDATE `servicios` SET  ser_idusuarioguia='$param31',`ser_idusuarioregistro`='$id_usuario',`ser_fechaguia`='$fechatiempo',ser_estado='9',ser_visto=0
-			WHERE `idservicios`='$idser' ";
-        // echo "⏱️ UPDATE servicios: " . round($t5 - $t4, 4) . " segundos<br>";
+        $t4 = microtime(true);
+        $sql2 = "UPDATE servicios SET ser_idusuarioguia = ?, ser_idusuarioregistro = ?, ser_fechaguia = ?, ser_estado = '9', ser_visto = 0 WHERE idservicios = ?";
+        $db->query($sql2, [$param31, $id_usuario, $fechatiempo, $idser]);
+        $t5 = microtime(true);
+        echo "⏱️ UPDATE servicios: " . round($t5 - $t4, 4) . " segundos<br>";
 
         // 🔍 UPDATE guias
-        // $t6 = microtime(true);
-        // $sql3 = "UPDATE guias SET gui_encomienda = ?, gui_fechaencomienda = ? WHERE gui_idservicio = ?";
-        // $db->query($sql3, [$id_nombre, $fechatiempo, $idser]);
-        // $t7 = microtime(true);
-		 echo$sql3="UPDATE `guias` SET `gui_encomienda`='$id_nombre',`gui_fechaencomienda`='$fechatiempo' WHERE `gui_idservicio`='$idser'";
-
-        // echo "⏱️ UPDATE guias: " . round($t7 - $t6, 4) . " segundos<br>";
+        $t6 = microtime(true);
+        $sql3 = "UPDATE guias SET gui_encomienda = ?, gui_fechaencomienda = ? WHERE gui_idservicio = ?";
+        $db->query($sql3, [$id_nombre, $fechatiempo, $idser]);
+        $t7 = microtime(true);
+        echo "⏱️ UPDATE guias: " . round($t7 - $t6, 4) . " segundos<br>";
 
         // 🔍 INSERT seguimientoruta
-        // $t8 = microtime(true);
-        // $sql4 = "INSERT INTO seguimientoruta (seg_fecha, seg_idservicio, seg_direccion, seg_tipo, seg_estado, seg_idusuario, seg_guia)
-        //          VALUES (?, ?, ?, 'Entrega', 'Asignada', ?, ?)";
-        // $db->query($sql4, [$fechatiempo, $idser, $direccion, $param31, $planilla]);
-        // $t9 = microtime(true);
-		echo$sqlse = "INSERT INTO `seguimientoruta`( `seg_fecha`, `seg_idservicio`, `seg_direccion`, `seg_tipo`, `seg_estado`,`seg_idusuario`,`seg_guia`) values ('$fechatiempo','$idser','$direccion','Entrega','Asignada','$param31','$planilla')";
-
-        // echo "⏱️ INSERT seguimientoruta: " . round($t9 - $t8, 4) . " segundos<br>";
+        $t8 = microtime(true);
+        $sql4 = "INSERT INTO seguimientoruta (seg_fecha, seg_idservicio, seg_direccion, seg_tipo, seg_estado, seg_idusuario, seg_guia)
+                 VALUES (?, ?, ?, 'Entrega', 'Asignada', ?, ?)";
+        $db->query($sql4, [$fechatiempo, $idser, $direccion, $param31, $planilla]);
+        $t9 = microtime(true);
+        echo "⏱️ INSERT seguimientoruta: " . round($t9 - $t8, 4) . " segundos<br>";
     }
 }
 
