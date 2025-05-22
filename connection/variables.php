45f92ef344c4d51f73386d8efe1237c8f55d4a8d
jarmandoR <jose523a@gmail.com>
2025-05-22 16:50:06 -0500

    wip:direccionamiento a bases de datos

--- connection/variables.php
+++ connection/variables.php
@@ -1,10 +1,27 @@
 <?php
+// //PRODUCCION
+// $bd="u713516042_transmillas2"; 
+// $host="localhost";
+// $user="u713516042_jose2";
+// $pass="Dobarli23@transmillas";
+// $Usu_ses="vive";
+// $salt = "transmi2344fsdfd";  
 
-$bd="u713516042_transmillas2"; 
+//PRUEBAS
+$bd="u713516042_transmillas"; 
 $host="localhost";
-$user="u713516042_jose2";
-$pass="Dobarli23@transmillas";
+$user="u713516042_jose";
+$pass="0?jBMSc4GUcN";
 $Usu_ses="vive";
-$salt = "transmi2344fsdfd";  
+$salt = "transmi2344fsdfd"; 
+
+
+//REMOTO PRUEBAS
+// $bd="u713516042_transmillas"; 
+// $host="45.14.88.1";
+// $user="u713516042_jose";
+// $pass="0?jBMSc4GUcN";
+// $Usu_ses="vive";
+// $salt = "transmi2344fsdfd"; 
 
 ?>
